63ba1fb238b03c7363d2db40cf46a290006cc162
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2023-08-07 15:01:48 +0530

    renamed:    script.js -> index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const count1=document.createElement("h1","myfunction");
+setTimeout((myfunction) => {
+    count1.innerText="10";
+    count1.setAttribute("id","count")
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="9";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="8";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="7";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="6";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="5";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="4";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="3";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="2";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerText="1";
+    document.body.appendChild(count1);
+    setTimeout((myfunction) => {
+    count1.innerHTML="Happy </br>Independence</br> Day";
+
+   })
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000); 
+}, 1000);
